9252d01eb3193bd90fa152863d22bd65a7f2f911
Artemii <fomygc@gmail.com>
2025-08-08 04:35:58 +0400

    env var fix

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -9,7 +9,15 @@ import (
 )
 
 func Run() error {
-	port := os.Getenv("TODO_PORT")
+	// Если переменная окр. существует, то стартуем на нем, иначе на 7540
+	port, ok := os.LookupEnv("TODO_PORT")
+	if !ok {
+		fmt.Println("Переменной окружения TODO_PORT не существует, сервер запущен на порту 7540")
+		port = "7540"
+	} else {
+		port = os.Getenv("TODO_PORT")
+	}
+
 	http.Handle("/", http.FileServer(http.Dir("web")))
 	api.Init()
 	return http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
